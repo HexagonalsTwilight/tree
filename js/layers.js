b31da70ba88ad3bfca5e889f08ea27cd230f9cf7
HexagonalsTwilight <haidangdangduy@gmail.com>
2026-08-09 10:34:18 +0700

    s

--- js/layers.js
+++ js/layers.js
@@ -4,12 +4,12 @@ addLayer("v", {
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(0),
+		points: new Decimal(1),
     }},
-    color: "#ff0000ff",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "Length", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    color: "rgb(70, 69, 69)",
+    requires: new Decimal(1), // Can be a function that takes requirement increases into account
+    resource: "viruses", // Name of prestige currency
+    baseResource: "cases", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -22,7 +22,7 @@ addLayer("v", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "v", description: "V: Reset for viruses", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })
--- js/mod.js
+++ js/mod.js
@@ -1,12 +1,12 @@
 let modInfo = {
 	name: "The Vorona Tree",
-	author: "Hexagonals Twilight",
+	author: "HexagonalsTwilight",
 	pointsName: "cases",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
+	initialStartPoints: new Decimal (0), // Used for hard resets and new players
 	offlineLimit: 1,  // In hours
 }
 
@@ -41,7 +41,7 @@ function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
 
-	let gain = new Decimal(1)
+	let gain = new Decimal(0)
 	return gain
 }
 
